c60d8b06424e8f180686f4a0e394af8b3f5328bf
jupiter <jupiter.fierce.31@gmail.com>
2019-10-13 01:26:13 +0800

    first commit

---
+++ server/config/index.js
@@ -0,0 +1,21 @@
+const validations = require('../../src/shared/model-validations');
+
+module.exports = {
+
+  // DB
+  // dbUri: 'mongodb://localhost/multi-tenant',
+  dbUri: 'mongodb://admin:Admin!2#4@ds333768.mlab.com:33768/helpdesk',
+  
+
+  logging: {
+    // dbUri: 'mongodb://localhost/multi-tenant-logs'
+    dbUri: 'mongodb://admin:Admin!2#4@ds333768.mlab.com:33768/helpdesklogs'
+   
+  },
+
+  // jsonwebtoken secret
+  jwtSecret: '!!secret phrase!!',
+
+  // Model validations
+  validations // :validations
+};
